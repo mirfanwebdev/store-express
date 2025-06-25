e815f2a07e8113d7aadc9b8181c25546ef558ec1
m.irfan <m.irfanwebdev@gmail.com>
2025-06-25 22:29:09 +0700

    setup api routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
+import apiRoutes from './routes';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api', apiRoutes);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to the Store API');
